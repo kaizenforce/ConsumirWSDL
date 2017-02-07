d0142c28258dbdec313948ae33bcd05f3438df47
jvargasipince <jvargas.ipince@gmail.com>
2017-02-06 20:20:13 -0500

    Add project files.

---
+++ ConsumiendoWebServices/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsumiendoWebServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Prueba.GlobalWeatherSoapClient ws = new Prueba.GlobalWeatherSoapClient();
+
+            string ListaCiudades = ws.GetCitiesByCountry("PERU");
+
+            Console.WriteLine(ListaCiudades);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsumirWSDL_WS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumirWSDL_WS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MiPrimerWS.GlobalWeatherSoapClient WS = new MiPrimerWS.GlobalWeatherSoapClient();
+
+           string ListaCiudadesPeru= WS.GetCitiesByCountry("PERU");
+
+           Console.WriteLine("Las ciudades del Perú son : " + ListaCiudadesPeru);
+
+           Console.WriteLine("");
+
+           string ListaCiudadesBrazil = WS.GetCitiesByCountry("BRAZIL");
+
+           Console.WriteLine("Las ciudades del Perú son : " + ListaCiudadesBrazil);
+
+           Console.WriteLine("");
+
+           Console.ReadKey();
+
+        }
+    }
+}
